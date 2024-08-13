90e996bb562d7ae599051b335691c9cf0efe075e
Jurjen Vorhauer <jurjen@vorhauer.nl>
2024-08-13 11:16:53 +0200

    refactored

--- index.ts
+++ index.ts
@@ -1,42 +1,4 @@
-import { unlinkSync, watch } from "node:fs"
-import { $, type ShellError } from "bun"
+import { formatDuration } from "date-fns"
 
-const here = process.env.ICA_DIR as string
-
-const watcher = watch(here, async (event, filename) => {
-	console.info(`Detected ${event} on ${here}/${filename}`)
-	if (filename?.endsWith(".ica")) {
-		const file = Bun.file(`${here}/${filename}`)
-		console.info(`Checking size of ${file.name}: ${file.size} bytes`)
-		if (file.size > 0) {
-			console.info(`Reading content of ${file.name}...`)
-			const text = await file.text()
-			const replaced = text.replace("DesktopViewer-ForceFullScreenStartup=On", "DesktopViewer-ForceFullScreenStartup=Off")
-			console.info(`Writing replaced content to /tmp/${filename}...`)
-			Bun.write(`/tmp/${filename}`, replaced)
-
-			console.info(`Opening /tmp/${filename} with default application...`)
-			try {
-				await $`open /tmp/${filename}`
-			} catch (e) {
-				const se = e as ShellError
-				console.error(`Opening ${filename} failed with exit code ${se.exitCode}`)
-				console.error(se.stderr.toString())
-				console.error(se.stdout.toString())
-			}
-
-			if (await file.exists()) {
-				console.info(`Deleting ${here}/${filename}...`)
-				unlinkSync(`${here}/${filename}`)
-			}
-		}
-	}
-})
-
-console.info(`Started watcher on ${here}`)
-
-process.on("SIGINT", () => {
-	console.info("Closing watcher...")
-	watcher.close()
-	process.exit(0)
-})
+const res = formatDuration({ seconds: 750 }, { format: ["hours", "minutes"] })
+console.info("res:", res)
--- parcl.ts
+++ parcl.ts
@@ -1,14 +1,16 @@
 import { unlinkSync } from "node:fs"
 import watcher from "@parcel/watcher"
 import { $, type ShellError } from "bun"
-import moment from "moment"
+import { format } from "date-fns"
 
 const here = process.env.ICA_DIR as string
+const DTF = "yyyy-MM-dd HH:mm:ss"
+const now = () => format(new Date(), DTF)
 
 const subs = await watcher.subscribe(here, async (_err, events) => {
 	for (const event of events) {
 		if (event.type === "create" && event.path.endsWith(".ica")) {
-			console.info(`${event.path} was created`)
+			console.info(`${now()} - ${event.path} was created`)
 			const file = Bun.file(event.path)
 			if (file.size > 0) {
 				const text = await file.text()
@@ -18,19 +20,17 @@ const subs = await watcher.subscribe(here, async (_err, events) => {
 					await $`open /tmp/${file.name}`
 				} catch (e) {
 					const se = e as ShellError
-					console.error(`Opening ${file.name} failed with exit code ${se.exitCode}`)
-					console.error(se.stderr.toString())
-					console.error(se.stdout.toString())
+					console.error(`${now()} - Opening ${file.name} failed with exit code ${se.exitCode}`)
 				}
 				unlinkSync(`${event.path}`)
 			}
 		}
 	}
 })
-console.info(`> ${moment().format("YYYY-MM-DD HH:mm:ss")} Started watcher on /Users/juvor/Downloads`)
+console.info(`> ${now()} Started watcher on /Users/juvor/Downloads`)
 
 process.on("SIGINT", async () => {
-	console.info("Closing watcher...")
+	console.info(`< ${now()} - Closing watcher...`)
 	await subs.unsubscribe()
 	process.exit(0)
 })
